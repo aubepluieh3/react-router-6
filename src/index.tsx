d01be0d37d2d49c9d2e982de1dcd51a539152cc4
soo <aubepluieh3@naver.com>
2022-11-15 15:04:04 +0900

    errorElement

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import App from "./App";
 import router from "./Router";
 
 const root = ReactDOM.createRoot(
